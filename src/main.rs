0a50873c00d2576275794c20373bfe35a2eafc05
Liwia Bukowska <44477673+liwiabukowska@users.noreply.github.com>
2023-10-16 23:21:25 +0200

    delete district names

--- src/main.rs
+++ src/main.rs
@@ -40,50 +40,6 @@ pub fn dhondt(results: &HashMap<String, f64>, seats: u64) -> Option<HashMap<Stri
     Some(assigned_seats)
 }
 
-const SEJM_DISTRICTS: [(&str, i32); 41]= [
-    ("Powiaty: bolesławiecki, głogowski, jaworski, jeleniogórski, kamiennogórski, legnicki, lubański, lubiński, lwówecki, polkowicki, zgorzelecki, złotoryjski, Miasta na prawach powiatu: Jelenia Góra, Legnica", 12),
-    ("Powiaty: dzierżoniowski, kłodzki, świdnicki, wałbrzyski, ząbkowicki", 8),
-    ("Powiaty: górowski, milicki, oleśnicki, oławski, strzeliński, średzki, trzebnicki, wołowski, wrocławski, Miasto na prawach powiatu: Wrocław", 14),
-    ("Powiaty: bydgoski, inowrocławski, mogileński, nakielski, sępoleński, świecki, tucholski, żniński, Miasto na prawach powiatu: Bydgoszcz", 12),
-    ("Powiaty: aleksandrowski, brodnicki, chełmiński, golubsko-dobrzyński, grudziądzki, lipnowski, radziejowski, rypiński, toruński, wąbrzeski, włocławski, Miasta na prawach powiatu: Grudziądz, Toruń, Włocławek", 13),
-    ("Powiaty: janowski, kraśnicki, lubartowski, lubelski, łęczyński, łukowski, opolski, puławski, rycki, świdnicki, Miasto na prawach powiatu: Lublin", 15),
-    ("Powiaty: bialski, biłgorajski, chełmski, hrubieszowski, krasnostawski, parczewski, radzyński, tomaszowski, włodawski, zamojski, Miasta na prawach powiatu: Biała Podlaska, Chełm, Zamość", 12),
-    ("Województwo lubuskie", 12),
-    ("Powiaty: brzeziński, łódzki wschodni, Miasto na prawach powiatu: Łódź", 10),
-    ("Powiaty: bełchatowski, opoczyński, piotrkowski, radomszczański, rawski, skierniewicki, tomaszowski, Miasta na prawach powiatu: Piotrków Trybunalski, Skierniewice", 9),
-    ("Powiaty: kutnowski, łaski, łęczycki, łowicki, pabianicki, pajęczański, poddębicki, sieradzki, wieluński, wieruszowski, zduńskowolski, zgierski", 12),
-    ("Powiaty: chrzanowski, myślenicki, oświęcimski, suski, wadowicki", 8),
-    ("Powiaty: krakowski, miechowski , olkuski, Miasto na prawach powiatu: Kraków", 14),
-    ("Powiaty: gorlicki, limanowski, nowosądecki, nowotarski, tatrzański, Miasto na prawach powiatu: Nowy Sącz", 10),
-    ("Powiaty: bocheński, brzeski, dąbrowski, proszowicki, tarnowski, wielicki, Miasto na prawach powiatu: Tarnów", 9),
-    ("Powiaty: ciechanowski, gostyniński, mławski, płocki, płoński, przasnyski, sierpecki, sochaczewski, żuromiński, żyrardowski, Miasto na prawach powiatu: Płock", 10),
-    ("Powiaty: białobrzeski, grójecki, kozienicki, lipski, przysuski, radomski, szydłowiecki, zwoleński, Miasto na prawach powiatu: Radom", 9),
-    ("Powiaty: garwoliński, łosicki, makowski, miński, ostrołęcki, ostrowski, pułtuski, siedlecki, sokołowski, węgrowski, wyszkowski, Miasta na prawach powiatu: Ostrołęka, Siedlce", 12),
-    ("Miasto na prawach powiatu: Warszawa (oraz obwody głosowania za granicą)", 20),
-    ("Powiaty: grodziski, legionowski, nowodworski, otwocki, piaseczyński, pruszkowski, warszawski zachodni, wołomiński", 12),
-    ("Województwo opolskie", 12),
-    ("Powiaty: bieszczadzki, brzozowski, jarosławski, jasielski, krośnieński, leski, lubaczowski, przemyski, przeworski, sanocki, Miasta na prawach powiatu: Krosno, Przemyśl", 11),
-    ("Powiaty: dębicki, kolbuszowski, leżajski, łańcucki, mielecki, niżański, ropczycko-sędziszowski, rzeszowski, stalowowolski, strzyżowski, tarnobrzeski, Miasta na prawach powiatu: Rzeszów, Tarnobrzeg", 15),
-    ("Województwo podlaskie", 14),
-    ("Powiaty: gdański, kwidzyński, malborski, nowodworski, starogardzki, sztumski, tczewski, Miasta na prawach powiatu: Gdańsk, Sopot (oraz obwody głosowania na polskich statkach morskich)", 12),
-    ("Powiaty: bytowski, chojnicki, człuchowski, kartuski, kościerski, lęborski, pucki, słupski, wejherowski, Miasta na prawach powiatu: Gdynia, Słupsk (oraz obwody głosowania na polskich statkach morskich)", 14),
-    ("Powiaty: bielski, cieszyński, pszczyński, żywiecki, Miasto na prawach powiatu: Bielsko-Biała", 9),
-    ("Powiaty: częstochowski, kłobucki, lubliniecki, myszkowski, Miasto na prawach powiatu: Częstochowa", 7),
-    ("Powiaty: gliwicki, tarnogórski, Miasta na prawach powiatu: Bytom, Gliwice, Zabrze", 9),
-    ("Powiaty: mikołowski, raciborski, rybnicki, wodzisławski, Miasta na prawach powiatu: Jastrzębie-Zdrój, Rybnik, Żory", 9),
-    ("Powiaty: bieruńsko-lędziński, Miasta na prawach powiatu: Chorzów, Katowice, Mysłowice, Piekary Śląskie, Ruda Śląska, Siemianowice Śląskie, Świętochłowice, Tychy", 12),
-    ("Powiaty: będziński, zawierciański, Miasta na prawach powiatu: Dąbrowa Górnicza, Jaworzno, Sosnowiec", 9),
-    ("Województwo świętokrzyskie", 16),
-    ("Powiaty: bartoszycki, braniewski, działdowski, elbląski, iławski, lidzbarski, nowomiejski, ostródzki, Miasto na prawach powiatu: Elbląg", 8),
-    ("Powiaty: ełcki, giżycki, gołdapski, kętrzyński, mrągowski, nidzicki, olecki, olsztyński, piski, szczycieński, węgorzewski, Miasto na prawach powiatu: Olsztyn", 10),
-    ("Powiaty: gostyński, jarociński, kaliski, kępiński, kościański, krotoszyński, leszczyński, ostrowski, ostrzeszowski, pleszewski, rawicki, Miasta na prawach powiatu: Kalisz, Leszno", 12),
-    ("Powiaty: gnieźnieński, kolski, koniński, słupecki, średzki, śremski, turecki, wrzesiński, Miasto na prawach powiatu: Konin", 9),
-    ("Powiaty: chodzieski, czarnkowsko-trzcianecki, grodziski, międzychodzki, nowotomyski, obornicki, pilski, szamotulski, wągrowiecki, wolsztyński, złotowski", 9),
-    ("Powiat: poznański, Miasto na prawach powiatu: Poznań", 10),
-    ("Powiaty: białogardzki, choszczeński, drawski, kołobrzeski, koszaliński, sławieński, szczecinecki, świdwiński, wałecki, Miasto na prawach powiatu: Koszalin", 8),
-    ("Powiaty: goleniowski, gryficki, gryfiński, kamieński, łobeski, myśliborski, policki, pyrzycki, stargardzki, Miasta na prawach powiatu: Szczecin, Świnoujście (oraz obwody głosowania na polskich statkach morskich)", 12),
-];
-
 fn main() {
     // populate dhondt from arguments : seats party1 votes1 party2 votes2 ...
     let (seats, results) = {
@@ -108,14 +64,19 @@ fn main() {
     };
 
     // println!("{:?}", results);
+    println!("without districts");
     let assigned_seats: Option<HashMap<String, u64>> = dhondt(&results, seats);
     println!("{:?}", assigned_seats);
 
     // for each district calculate number of seats with dhondt method
-    let mut districts: HashMap<String, HashMap<String, u64>> = HashMap::new();
-    for (district, seats) in SEJM_DISTRICTS.iter() {
-        let assigned_seats = dhondt(&results, *seats as u64).unwrap();
-        districts.insert(district.to_string(), assigned_seats);
+    const SEJM_DISTRICT_MANDATES: [u64; 41] = [
+        12, 8, 14, 12, 13, 15, 12, 12, 10, 9, 12, 8, 14, 10, 9, 10, 9, 12, 20, 12, 12, 11, 15, 14,
+        12, 14, 9, 7, 9, 9, 12, 9, 16, 8, 10, 12, 9, 9, 10, 8, 12,
+    ];
+    let mut districts: HashMap<usize, HashMap<String, u64>> = HashMap::new();
+    for (district, &seats) in SEJM_DISTRICT_MANDATES.iter().enumerate() {
+        let assigned_seats = dhondt(&results, seats).unwrap();
+        districts.insert(district, assigned_seats);
     }
 
     // println!("{:?}", districts);
@@ -128,6 +89,6 @@ fn main() {
         }
     }
 
+    println!("with districts");
     println!("{:?}", total_seats);
-
 }
